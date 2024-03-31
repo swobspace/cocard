a119a9d57361bcd1292527b92bb8c438290f8a78
Wolfgang Barth <wob@swobspace.net>
2024-03-31 18:05:57 +0200

    rake db:migrate

---
+++ db/schema.rb
@@ -0,0 +1,84 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_03_30_095251) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "wobauth_authorities", force: :cascade do |t|
+    t.bigint "authorizable_id"
+    t.string "authorizable_type"
+    t.bigint "role_id"
+    t.bigint "authorized_for_id"
+    t.string "authorized_for_type"
+    t.date "valid_from"
+    t.date "valid_until"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["authorizable_id"], name: "index_wobauth_authorities_on_authorizable_id"
+    t.index ["authorized_for_id"], name: "index_wobauth_authorities_on_authorized_for_id"
+    t.index ["role_id"], name: "index_wobauth_authorities_on_role_id"
+  end
+
+  create_table "wobauth_groups", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "wobauth_memberships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "group_id"
+    t.boolean "auto", default: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["group_id"], name: "index_wobauth_memberships_on_group_id"
+    t.index ["user_id"], name: "index_wobauth_memberships_on_user_id"
+  end
+
+  create_table "wobauth_roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "wobauth_users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.text "gruppen"
+    t.string "sn"
+    t.string "givenname"
+    t.string "displayname"
+    t.string "telephone"
+    t.string "active_directory_guid"
+    t.string "userprincipalname"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.string "title", default: ""
+    t.string "position", default: ""
+    t.string "department", default: ""
+    t.string "company", default: ""
+    t.index ["reset_password_token"], name: "index_wobauth_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_wobauth_users_on_username", unique: true
+  end
+
+end
