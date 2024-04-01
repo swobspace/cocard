b4d7e393ed6cc5debb7358c43fb79553383ab730
Wolfgang Barth <wob@swobspace.net>
2024-04-01 11:33:32 +0200

    add basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,31 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
+
+# -- load YAML config data
+config = YAML.load_file(File.join(Rails.root, 'db', 'seeds.yml'))
+
+# -- Roles from yaml(:uniqueness => true)
+Wobauth::Role.create(config['roles'])
+
+# -- start with Admin
+admin_role = Wobauth::Role.find_by(name: 'Admin')
+
+Wobauth::User.transaction do
+  admin = Wobauth::User.find_or_create_by(username: 'admin') do |adm|
+    adm.sn = 'Admin'
+    adm.givenname = 'Meister'
+    adm.password = 'admin9999'
+    adm.password_confirmation = 'admin9999'
+  end
+
+  Wobauth::Authority.transaction do
+    a = Wobauth::Authority.find_or_create_by(authorizable: admin, role: admin_role)
+    raise ActiveRecord::Rollback if a.nil?
+  end
+end
+
+# only for datasets with uniq attribute
+[].each do |myklass|
+  mytable = myklass.name.underscore.pluralize
+  config = YAML.load_file(File.join(Rails.root, 'db', "#{mytable}.yml"))
+  myklass.create(config[mytable])
+end
