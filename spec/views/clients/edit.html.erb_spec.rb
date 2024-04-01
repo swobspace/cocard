d50d47f1b9e7a21d5bf86681956c28bb35c00e10
Wolfgang Barth <wob@swobspace.net>
2024-04-01 13:45:36 +0200

    scaffolding Client

---
+++ spec/views/clients/edit.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "clients/edit", type: :view do
+  let(:client) {
+    Client.create!(
+      name: "MyString",
+      description: "MyString"
+    )
+  }
+
+  before(:each) do
+    assign(:client, client)
+  end
+
+  it "renders the edit client form" do
+    render
+
+    assert_select "form[action=?][method=?]", client_path(client), "post" do
+
+      assert_select "input[name=?]", "client[name]"
+
+      assert_select "input[name=?]", "client[description]"
+    end
+  end
+end
---
+++ spec/views/clients/index.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "clients/index", type: :view do
+  before(:each) do
+    assign(:clients, [
+      Client.create!(
+        name: "Name",
+        description: "Description"
+      ),
+      Client.create!(
+        name: "Name",
+        description: "Description"
+      )
+    ])
+  end
+
+  it "renders a list of clients" do
+    render
+    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
+    assert_select cell_selector, text: Regexp.new("Name".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("Description".to_s), count: 2
+  end
+end
