eaec33cf62ee41cffe4d7d5152ee32e045022635
Wolfgang Barth <wob@swobspace.net>
2024-04-01 18:48:14 +0200

    first connector form

--- app/models/connector.rb
+++ app/models/connector.rb
@@ -1,2 +1,11 @@
 class Connector < ApplicationRecord
+  # -- associations
+  has_and_belongs_to_many :locations
+  has_and_belongs_to_many :clients
+
+  # -- configuration
+  has_rich_text :description
+
+  # -- validations and callbacks
+  validates :ip, presence: true, uniqueness: true
 end
