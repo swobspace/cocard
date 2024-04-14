38180e61b3f2da15a3dc2b826806b604b9b73ba5
Wolfgang Barth <wob@swobspace.net>
2024-04-14 11:47:39 +0200

    spec for connector_services/fetch

--- app/services/connector_services/fetch.rb
+++ app/services/connector_services/fetch.rb
@@ -24,19 +24,20 @@ module ConnectorServices
       error_messages = []
       response = Faraday.get(connector.sds_url)
       unless response.success?
-        error_messages << response.headers
+        error_messages << response.headers.join(' ')
         error_messages << response.status
         error_messages << response.body
-        return Result.new(success: false, error_messages: error_messages, sds: nil)
+        return Result.new(success?: false, error_messages: error_messages, sds: nil)
       end
 
       sds = Cocard::SDS.new(response.body)
       if sds.nil?
         error_messages << "No SDS retrieved"
-        return Result.new(success: false, error_messages: error_messages, sds: nil)
+        return Result.new(success?: false, error_messages: error_messages, sds: nil)
       end
 
       connector.update(connector_services: sds.connector_services)
+      Result.new(success?: true, error_messages: error_messages, sds: sds.connector_services)
     end
   end
 end
---
+++ spec/services/connector_services/fetch_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+module ConnectorServices
+  RSpec.describe Fetch do
+    Fake = Struct.new(:headers, :success?, :status, :body)
+    let(:sds_file) { File.join(Rails.root, 'spec', 'fixtures', 'files', 'connector.sds') }
+    let(:sds_string)      { File.read(sds_file) }
+    let(:connector) do
+      FactoryBot.create(:connector,
+        ip: ENV['SDS_IP'],
+        sds_url: ENV['SDS_URL']
+      )
+    end
+    subject { ConnectorServices::Fetch.new( connector: connector) }
+
+
+    # check for instance methods
+    describe 'check if instance methods exists' do
+      it { expect(subject.respond_to?(:call)).to be_truthy }
+    end
+
+    describe '#call' do
+      describe 'with status 200' do
+        let(:result) { subject.call }
+        let(:fake) do
+          Fake.new([], true, 200, sds_string)
+        end
+
+        before(:each) do
+          expect(Faraday).to receive(:get).with(any_args).and_return(fake)
+        end
+
+        it { expect(result.success?).to be_truthy }
+        it { expect(result.respond_to?(:error_messages)).to be_truthy }
+        it { expect(result.respond_to?(:sds)).to be_truthy }
+        it { expect(result.error_messages.present?).to be_falsey }
+        it { expect(result.sds).to be_present }
+        it { expect(result.sds.keys).to include( "TLSMandatory", "ClientAutMandatory",
+                                          "ProductInformation", "ServiceInformation") }
+      end
+
+      describe 'with status 500' do
+        let(:result) { subject.call }
+        let(:fake) do
+          Fake.new(['nonsense'], false, 500, 'no information available')
+        end
+
+        before(:each) do
+          expect(Faraday).to receive(:get).with(any_args).and_return(fake)
+        end
+
+        it { expect(result.success?).to be_falsey }
+        it { expect(result.respond_to?(:error_messages)).to be_truthy }
+        it { expect(result.respond_to?(:sds)).to be_truthy }
+        it { expect(result.error_messages).to include('nonsense', 
+                                                      'no information available') }
+        it { expect(result.sds).to be_nil }
+      end
+    end
+  end
+end
