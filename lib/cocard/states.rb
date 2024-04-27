4f3ea4f010c1b374088683eeba9fe6c96d819781
Wolfgang Barth <wob@swobspace.net>
2024-04-27 12:41:10 +0200

    Cocard::State module

---
+++ lib/cocard/states.rb
@@ -0,0 +1,12 @@
+module Cocard
+  module State
+    #
+    # Global states
+    #
+    NOTHING  = -1.freeze
+    OK       = 0.freeze
+    WARNING  = 1.freeze
+    CRITICAL = 2.freeze
+    UNKNOWN  = 3.freeze
+  end
+end
