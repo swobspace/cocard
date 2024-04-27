3b80c3ee58cef66751ac1374402d678a58381ced
Wolfgang Barth <wob@swobspace.net>
2024-04-27 12:40:45 +0200

    working on get_resource_information

--- app/services/cocard/get_resource_information.rb
+++ app/services/cocard/get_resource_information.rb
@@ -15,7 +15,11 @@ module Cocard
     #
     def initialize(options = {})
       options.symbolize_keys
-      @context = options.fetch(:context)
+      @connector_context = options.fetch(:connector_context)
+      @connector = @connector_context.connector
+      @mandant   = @connector_context.context.mandant
+      @client_system  = @connector_context.context.client_system
+      @workplace = @connector_context.context.workplace
     end
 
     # service.call()
@@ -23,10 +27,10 @@ module Cocard
     def call
       error_messages = []
       result = Cocard::SOAP::GetResourceInformation.new(
-                 connector: context.connector,
-                 mandant: context.mandant,
-                 client_system: context.client_system,
-                 workplace: context.workplace).call
+                 connector: connector,
+                 mandant: mandant,
+                 client_system: client_system,
+                 workplace: workplace).call
       unless result.success?
         return Result.new(success?: false, error_messages: result.error_messages, 
                           resource_information: nil)
@@ -37,5 +41,7 @@ module Cocard
                  resource_information: resource_information)
     end
 
+  private
+    attr_reader :connector, :context, :workplace, :mandant, :client_system
   end
 end
---
+++ spec/services/cocard/get_resource_information_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+module Cocard
+  RSpec.describe GetResourceInformation do
+    # 
+    #  create fake response for Cocard::Soap::GetResourceInformation
+    #
+    Fake = Struct.new(:success?, :error_messages, :response)
+    let(:connector_yml) do
+      File.join(Rails.root, 'spec', 'fixtures', 'files', 'connector.sds')
+    end
+
+    let(:resource_yml) do
+      file = File.join(Rails.root, 'spec', 'fixtures', 'files', 'get_resource_information_response.yml')
+      File.read(file)
+    end
+
+    let(:fake_ok)  { Fake.new(true, [], 
+                              YAML.unsafe_load(resource_yml)[:get_resource_information_response]) }
+    let(:fake_err) { Fake.new(false, ['something is wrong'], nil) }
+
+    #
+    # connector and context
+    #
+    let(:connector) do
+      FactoryBot.create(:connector,
+        ip: ENV['SDS_IP'],
+        connector_services: YAML.load_file(connector_yml)
+      )
+    end
+
+    let(:context) { FactoryBot.create(:context) }
+    let(:connector_context) do
+      ConnectorContext.create!(connector: connector, context: context)
+    end
+
+    subject do
+      Cocard::GetResourceInformation.new(connector_context: connector_context)
+    end
+
+    it "does not raise an NotImplementedError" do
+      expect { subject }.not_to raise_error
+    end
+
+    # check for instance methods
+    describe 'check if instance methods exists' do
+      it { expect(subject.respond_to?(:call)).to be_truthy }
+    end
+
+    describe '#call' do
+      let(:soap) { instance_double(Cocard::SOAP::GetResourceInformation) }
+      describe "return error if not successful" do
+        before(:each) do
+          expect(Cocard::SOAP::GetResourceInformation).to receive(:new).and_return(soap)
+          expect(soap).to receive(:call).and_return(fake_err)
+        end
+        it { expect(subject.call.success?).to be_falsey }
+      end
+
+      describe "successful call" do
+        before(:each) do
+          expect(Cocard::SOAP::GetResourceInformation).to receive(:new).and_return(soap)
+          expect(soap).to receive(:call).and_return(fake_ok)
+        end
+        it { expect(subject.call.success?).to be_truthy }
+        it { expect(subject.call.resource_information).to be_kind_of(Cocard::ResourceInformation) }
+      end
+    end
+  end
+end
