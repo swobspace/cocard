557b269be93a04c69dae5dd4eeec2fb53d58a298
Wolfgang Barth <wob@swobspace.net>
2024-05-05 10:45:37 +0200

    Cards::Creator ready

---
+++ app/services/cards/creator.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+module Cards
+  #
+  # Create or update Card
+  #
+  class Creator
+    attr_reader :channel
+
+    # creator = Card::Creator(options)
+    #
+    # mandantory options:
+    # * :connector  - connector object
+    # * :cc         - Cocard::Card object
+    #
+    def initialize(options = {})
+      options.symbolize_keys
+      @connector = options.fetch(:connector)
+      @cc       = options.fetch(:cc)
+    end
+
+    # rubocop:disable Metrics/AbcSize, Rails/SkipsModelValidations
+    def save
+      #
+      # don't store egk and kvk
+      #
+      return false if cc.iccsn.blank?
+
+      #
+      # get card_terminal
+      #
+
+      ct = CardTerminal.where(connector_id: connector.id, ct_id: cc.ct_id).first
+      
+      @card = Card.find_or_initialize_by(iccsn: cc.iccsn) do |card|
+                         Cocard::Card::ATTRIBUTES.each do |attr|
+                           next if attr == :iccsn
+                           card.send("#{attr}=", cc.send(attr))
+                         end
+                         card.card_terminal_id = ct.id
+                       end
+
+      if @card.persisted?
+        Cocard::Card::ATTRIBUTES.each do |attr|
+          next if attr == :mac
+          @card.send("#{attr}=", cc.send(attr))
+        end
+        @card.card_terminal = ct
+      end
+
+      if @card.save
+        @card.touch
+      else
+        Rails.logger.warn("WARN:: could not create or save card #{@card.mac}: " +
+          @card.errors.full_messages.join('; '))
+        false
+      end
+    end
+    # rubocop:enable Metrics/AbcSize, Rails/SkipsModelValidations
+
+    private
+
+    attr_reader :cc, :connector
+
+  end
+end
---
+++ spec/services/cards/creator_spec.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+module Cards
+  RSpec.describe Creator do
+    let(:connector) { FactoryBot.create(:connector) }
+    let!(:ct) do
+      FactoryBot.create(:card_terminal, 
+        connector_id: connector.id,
+        ct_id: 'CT_ID_0176'
+      )
+    end
+    let!(:ts) { 1.day.before(Time.current) }
+    let(:card_hash) do
+       {:card_handle=>"ee676b27-5b40-4a40-9c65-979cc3113a1e",
+        :card_type=>"SMC-B",
+        :card_version=>
+         {:cos_version=>{:major=>"4", :minor=>"5", :revision=>"0"},
+          :object_system_version=>{:major=>"4", :minor=>"8", :revision=>"0"},
+          :atr_version=>{:major=>"2", :minor=>"0", :revision=>"0"},
+          :gdo_version=>{:major=>"1", :minor=>"0", :revision=>"0"}},
+        :iccsn=>"80276002711000000000",
+        :ct_id=>"CT_ID_0176",
+        :slot_id=>"1",
+        :insert_time=>ts,
+        :card_holder_name=>"Doctor Who's Universe",
+        :certificate_expiration_date=>1.year.after(Date.current)}
+    end
+
+    let(:cc) { Cocard::Card.new(card_hash) }
+
+    subject { Cards::Creator.new(connector: connector, cc: cc) }
+
+    # check for instance methods
+    describe 'check if instance methods exists' do
+      it { expect(subject).to be_kind_of(Cards::Creator) }
+      it { expect(subject.respond_to?(:save)).to be_truthy }
+    end
+
+    describe '#new' do
+      context 'without :cc' do
+        it 'raises a KeyError' do
+          expect do
+            Creator.new()
+          end.to raise_error(KeyError)
+        end
+      end
+    end
+
+    describe '#save' do
+      context 'new card' do
+        it 'create a new card' do
+          expect do
+            subject.save
+          end.to change(Card, :count).by(1)
+        end
+      end
+
+      context 'with an existing card' do
+        let!(:card) do
+          FactoryBot.create(:card, iccsn: '80276002711000000000')
+        end
+        before(:each) { card.reload }
+        it 'does not create a card' do
+          expect {
+            subject.save
+          }.to change(Card, :count).by(0)
+        end
+
+        it { expect(subject.save).to be_truthy }
+
+        context 'update attributes' do
+          before(:each) do
+            subject.save
+            card.reload
+          end
+          it { expect(card.card_terminal).to eq(ct) }
+          it { expect(card.name).to eq("") }
+          it { expect(card.description.to_plain_text).to eq("") }
+          it { expect(card.card_handle).to eq("ee676b27-5b40-4a40-9c65-979cc3113a1e") }
+          it { expect(card.card_type).to eq("SMC-B") }
+          it { expect(card.iccsn).to eq("80276002711000000000") }
+          it { expect(card.slotid).to eq(1) }
+          it { expect(card.insert_time.floor).to eq(ts.floor) }
+          it { expect(card.card_holder_name).to eq("Doctor Who's Universe") }
+          it { expect(card.expiration_date).to eq(1.year.after(Date.current)) }
+        end
+      end
+    end
+  end
+end
