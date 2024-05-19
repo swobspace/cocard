2adb475b32049c78e173aadcde930534b66820e6
Wolfgang Barth <wob@swobspace.net>
2024-05-19 19:20:53 +0200

    Card::ExpirationDateComponent

---
+++ app/components/card/expiration_date_component.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Card::ExpirationDateComponent < ViewComponent::Base
+  def initialize(card:)
+    @card = card
+  end
+
+  def message
+    if card.expiration_date.blank?
+      Cocard::States.flag(Cocard::States::NOTHING)
+    elsif card.expiration_date >= 3.month.after(Date.current)
+      Cocard::States.flag(Cocard::States::OK) + " #{card.expiration_date}"
+    elsif card.expiration_date >= Date.current
+      Cocard::States.flag(Cocard::States::WARNING) + " #{card.expiration_date}"
+    else
+      Cocard::States.flag(Cocard::States::CRITICAL) + " #{card.expiration_date}"
+    end
+  end
+
+private
+  attr_reader :card
+
+
+
+end
