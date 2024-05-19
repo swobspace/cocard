d12e279fb3cd1ad793d98522880e6659c0ab076d
Wolfgang Barth <wob@swobspace.net>
2024-05-19 18:55:17 +0200

    unicode symbol for states

--- lib/cocard/states.rb
+++ lib/cocard/states.rb
@@ -8,5 +8,16 @@ module Cocard
     WARNING  = 1.freeze
     CRITICAL = 2.freeze
     UNKNOWN  = 3.freeze
+
+    def self.flag(state)
+      case state
+      when UNKNOWN then "\u2753"
+      when CRITICAL then "\u274C"
+      when WARNING then "\u26A1"
+      when OK then "\u2705"
+      else
+        ""
+      end
+    end
   end
 end
