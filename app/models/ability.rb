17448bef2c9b2ea86df3470a1bca0873b693c345
Wolfgang Barth <wob@swobspace.net>
2024-05-22 18:13:15 +0200

    add simple authorization model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,17 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    alias_action :search, :search_form, :query, :to => :read
+    alias_action :sindex, :to => :read
+
     @user = user
     if @user.nil?
       can :read, Home
-    else
+    elsif @user.is_admin?
       can :manage, :all
       cannot %i[update destroy], :roles, ro: true
+    elsif @user.role?(:reader)
+      can [:read, :navigate], :all
     end
   end
 end
