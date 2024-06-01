955f12ae2fef4d26a0648283e3593b62f3425d7a
Wolfgang Barth <wob@swobspace.net>
2024-06-01 12:54:59 +0200

    Network::best_match to find matching location

--- app/models/concerns/network_concerns.rb
+++ app/models/concerns/network_concerns.rb
@@ -2,6 +2,19 @@ module NetworkConcerns
   extend ActiveSupport::Concern
 
   included do
+    def self.best_match(ip)
+      return [] if ip.nil?
+      networks = Network.
+                   where("? << netzwerk", ip.to_s).
+                   order(Arel.sql("masklen(netzwerk) desc"))
+      if networks.any?
+        masklen = networks.first.netzwerk.prefix
+        networks.where("masklen(netzwerk) >= ?", masklen)
+      else
+        []
+      end
+    end
+
   end
 
   def to_cidr_s
--- spec/models/concerns/network_concerns_spec.rb
+++ spec/models/concerns/network_concerns_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe NetworkConcerns, type: :model do
     it { expect(network.to_cidr_s).to eq('192.0.2.32/28') }
   end
 
+  describe "::best_match(ip)" do
+    let!(:n1) { FactoryBot.create(:network, netzwerk: '192.0.2.0/24') }
+    let!(:n2) { FactoryBot.create(:network, netzwerk: '192.0.2.32/27') }
+    let!(:n3) { FactoryBot.create(:network, netzwerk: '192.168.0.0/24') }
+    let!(:n4) { FactoryBot.create(:network, netzwerk: '192.168.0.0/25') }
+
+    it { expect(Network.best_match('192.0.2.35')).to contain_exactly(n2) }
+    it { expect(Network.best_match('192.0.2.17')).to contain_exactly(n1) }
+    it { expect(Network.best_match('192.168.0.17')).to contain_exactly(n4) }
+    it { expect(Network.best_match('198.51.100.4')).to contain_exactly() }
+    it { expect(Network.best_match(nil)).to contain_exactly() }
+  end
+
 end
