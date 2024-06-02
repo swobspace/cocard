362fdc646c26de953cdd2f1860f9775759eb9941
Wolfgang Barth <wob@swobspace.net>
2024-06-02 14:13:11 +0200

    TerminalWorkplaces::Crupdator

---
+++ app/services/terminal_workplaces/crupdator.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+module TerminalWorkplaces
+  #
+  # Creates a workplace if not not exist
+  # creates or updates a terminal_workplace with scope [:mandant, :client_system]
+  # deletes obsolete terminal_workplaces with scope [:mandant, :client_system]
+  #
+  class Crupdator
+    attr_reader :workplaces, :client_system, :mandant, :card_terminal
+
+    # crupd = TerminalWorkplaces::Crupdator(options)
+    #
+    # mandantory options:
+    # * :card_terminal - object
+    # * :mandant       - string
+    # * :client_system - string
+    # * :workplaces    - Array of strings
+    #
+    # obsolete_workplaces = crupd.call 
+    #
+    def initialize(options = {})
+      options.symbolize_keys
+      @card_terminal  = options.fetch(:card_terminal)
+      @mandant        = options.fetch(:mandant)
+      @client_system  = options.fetch(:client_system)
+      @wp_names        = Array(options.fetch(:workplaces))
+    end
+
+    # rubocop:disable Metrics/AbcSize, Rails/SkipsModelValidations
+    def call
+      @workplaces = find_or_create_workplaces_by_name(wp_names)
+      @workplaces.each do |wp|
+        TerminalWorkplace.find_or_create_by(
+          card_terminal_id: card_terminal.id,
+          mandant: mandant,
+          client_system: client_system,
+          workplace_id: wp.id
+        )
+      end
+      obsolete = obsolete_terminal_workplaces(@workplaces)
+      TerminalWorkplace.destroy(obsolete.map(&:id))
+      obsolete.map{|o| o.workplace}
+    end
+    # rubocop:enable Metrics/AbcSize, Rails/SkipsModelValidations
+
+  private
+    attr_reader :wp_names
+
+    def find_or_create_workplaces_by_name(wp_names)
+      [].tap do |wp|
+        wp_names.each do |name|
+          wp << Workplace.find_or_create_by(name: name)
+        end
+      end
+    end
+
+    def obsolete_terminal_workplaces(workplaces)
+      card_terminal.terminal_workplaces
+                   .where(mandant: mandant, client_system: client_system)
+                   .reject{|twp| workplaces.include?(twp.workplace)}
+    end
+  end
+end
---
+++ spec/services/terminal_workplaces/crupdator_spec.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+module TerminalWorkplaces
+  RSpec.describe Crupdator do
+    # let!(:ts) { Time.current }
+    let(:ct)  { FactoryBot.create(:card_terminal) }
+
+    subject do 
+      TerminalWorkplaces::Crupdator.new(
+        card_terminal: ct,
+        mandant: "Mandy",
+        client_system: "SlowMed",
+        workplaces: ['PC002', 'PC003']
+      )
+    end
+
+    # check for instance methods
+    describe 'check if instance methods exists' do
+      it { expect(subject).to be_kind_of(TerminalWorkplaces::Crupdator) }
+      it { expect(subject.respond_to?(:call)).to be_truthy }
+    end
+
+    describe '#new' do
+      context 'without params' do
+        it 'raises a KeyError' do
+          expect do
+            Crupdator.new()
+          end.to raise_error(KeyError)
+        end
+      end
+    end
+
+    describe '#call' do
+      context 'with new workplaces' do
+        it 'create new workplaces' do
+          expect do
+            subject.call
+          end.to change(Workplace, :count).by(2)
+        end
+
+        it 'creates new terminal_workplaces' do
+          expect do
+            subject.call
+          end.to change(TerminalWorkplace, :count).by(2)
+        end
+      end
+
+      context "with existing workplaces" do
+        let!(:wp1) { FactoryBot.create(:workplace, name: 'PC001') }
+        let!(:wp2) { FactoryBot.create(:workplace, name: 'PC002') }
+        let!(:wp3) { FactoryBot.create(:workplace, name: 'PC003') }
+
+        context "with no existing terminal_workplaces" do
+          it 'does not create new workplaces' do
+            expect do
+              subject.call
+            end.to change(Workplace, :count).by(0)
+          end
+
+          it 'creates new terminal_workplaces' do
+            expect do
+              subject.call
+            end.to change(TerminalWorkplace, :count).by(2)
+          end
+        end
+
+        context "with existing terminal_workplaces" do
+          let!(:twp1)  do
+            FactoryBot.create(:terminal_workplace, 
+              card_terminal: ct,
+              mandant: 'Mandy',
+              client_system: 'SlowMed',
+              workplace: wp1
+            )
+          end
+          let!(:twp2)  do
+            FactoryBot.create(:terminal_workplace, 
+              card_terminal: ct,
+              mandant: 'Mandy',
+              client_system: 'SlowMed',
+              workplace: wp2
+            )
+          end
+
+          it 'creates new terminal_workplaces' do
+            expect do
+              subject.call
+            end.to change(TerminalWorkplace, :count).by(0)
+          end
+
+          it 'adds one terminal_workplace and deletes another' do
+            expect(TerminalWorkplace.all).to contain_exactly(twp1, twp2)
+            result = subject.call
+            expect(result).to contain_exactly(wp1)
+            expect(TerminalWorkplace.all).to include(twp2)
+            expect(TerminalWorkplace.all).not_to include(twp1)
+            expect(TerminalWorkplace.count).to eq(2)
+          end
+        end
+      end
+    end
+  end
+end
