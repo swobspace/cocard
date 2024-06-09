572145ccf54514366973feb297ecf774da9f85b0
Wolfgang Barth <wob@swobspace.net>
2024-06-09 10:21:52 +0200

    bump version to 0.1.0

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,6 +1,4 @@
 module Cocard
-  VERSION     = '0.0.1-dev'.freeze
-  RELEASEDATE = '2024-03-31'.freeze
+  VERSION     = '0.1.0'.freeze
+  RELEASEDATE = '2024-06-09'.freeze
 end
-
-### DONT FORGET app/views/pages/Changelog.adoc ###
