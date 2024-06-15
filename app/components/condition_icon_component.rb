a238b8c368380dfdc4971edf1115388b0393846b
Wolfgang Barth <wob@swobspace.net>
2024-06-15 18:06:50 +0200

    choice for text or icon

--- app/components/condition_icon_component.rb
+++ app/components/condition_icon_component.rb
@@ -5,10 +5,12 @@
 # item must respond_to :condition, :condition_text
 #
 class ConditionIconComponent < ViewComponent::Base
-  def initialize(item:, small: false, period: Cocard::grace_period)
-    @item = item
+  def initialize(item:, small: false, period: Cocard::grace_period, as_text: false)
+    @item  = item
     @button_class = ""
     @icon  = ""
+    @text  = ""
+    @as_text  = as_text
     @small = small
     @period = period
     set_variant
@@ -22,26 +24,31 @@ class ConditionIconComponent < ViewComponent::Base
     end
 
     case item.condition
-     when Cocard::States::CRITICAL
+      when Cocard::States::CRITICAL
         @button_class = "#{size} #{btn}-danger"
-        @icon  = "fa-solid fa-circle-exclamation"
+        @icon = "fa-solid fa-circle-exclamation"
+        @text = "CRITICAL"
       when Cocard::States::UNKNOWN
         @button_class = "#{size} #{btn}-info"
-        @icon  = "fa-solid fa-circle-question #{textcolor}"
+        @icon = "fa-solid fa-circle-question #{textcolor}"
+        @text = "UNKNOWN"
       when Cocard::States::WARNING
         @button_class = "#{size} #{btn}-warning"
-        @icon  = "fa-solid fa-triangle-exclamation #{textcolor}"
+        @icon = "fa-solid fa-triangle-exclamation #{textcolor}"
+        @text = "WARNING"
       when Cocard::States::OK
         @button_class = "#{size} #{btn}-success"
-        @icon  = "fa-solid fa-circle-check"
+        @icon = "fa-solid fa-circle-check"
+        @text = "OK"
       else
         @button_class = "#{size} #{btn}-secondary"
-        @icon  = "fa-solid fa-circle-xmark"
+        @icon = "fa-solid fa-circle-xmark"
+        @text = "NOTHING"
     end
   end
 
 private
-  attr_reader :item, :button_class, :icon, :period
+  attr_reader :item, :button_class, :icon, :period, :as_text, :text
 
   def outdated?
     item.updated_at < period.before(Time.current)
--- spec/components/condition_icon_component_spec.rb
+++ spec/components/condition_icon_component_spec.rb
@@ -14,6 +14,24 @@ RSpec.describe ConditionIconComponent, type: :component do
     end
   end
 
+  describe "with condition = WARNING" do
+    it "shows WARNING button" do
+      expect(connector).to receive(:condition).at_least(:once)
+                                              .and_return(Cocard::States::WARNING)
+      render_inline(described_class.new(item: connector))
+      expect(page).to have_css('button[class="btn btn-warning"]')
+    end
+  end
+
+  describe "with condition = OK" do
+    it "shows OK button" do
+      expect(connector).to receive(:condition).at_least(:once)
+                                              .and_return(Cocard::States::OK)
+      render_inline(described_class.new(item: connector))
+      expect(page).to have_css('button[class="btn btn-success"]')
+    end
+  end
+
   describe "with condition = UNKNOWN" do
     it "shows UNKNOWN button" do
       expect(connector).to receive(:condition).at_least(:once)
