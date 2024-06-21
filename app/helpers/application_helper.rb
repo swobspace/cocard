f0ca1ea5079f36064694ed332cbef0d14360a490
Wolfgang Barth <wob@swobspace.net>
2024-06-21 17:46:44 +0200

    fix active class for navi

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
   include Pagy::Frontend
   include Wobapphelpers::Helpers::All
+
+  def configuration_active_class
+    if Cocard::CONFIGURATION_CONTROLLER.include?(controller.controller_name.to_s)
+      "active"
+    end
+  end
+
 end
