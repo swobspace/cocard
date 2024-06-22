c21485a02b30972fa59e85b20ca3d77b56651923
Wolfgang Barth <wob@swobspace.net>
2024-06-22 14:32:39 +0200

    fix some logik

--- app/services/cocard/get_cards.rb
+++ app/services/cocard/get_cards.rb
@@ -61,7 +61,6 @@ module Cocard
     attr_reader :connector, :context, :workplace, :mandant, :client_system
 
     def log_error(message)
-      message = "No message" if message.blank?
       logger = Logs::Creator.new(loggable: connector, level: 'ERROR',
                                  action: 'GetCards', message: message)
       unless logger.call(message.blank?)
