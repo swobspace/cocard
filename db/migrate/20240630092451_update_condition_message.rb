2cf775c1430737d7f015fad52da74f6f0d8219d5
Wolfgang Barth <wob@swobspace.net>
2024-06-30 11:35:07 +0200

    make migration more robust

--- db/migrate/20240630092451_update_condition_message.rb
+++ db/migrate/20240630092451_update_condition_message.rb
@@ -4,6 +4,7 @@ class UpdateConditionMessage < ActiveRecord::Migration[7.1]
   #
   def up
     (Connector.all + CardTerminal.all + Card.all).each do |c|
+      next unless c.condition_message == '-'
       c.update_condition
       c.save
     end
