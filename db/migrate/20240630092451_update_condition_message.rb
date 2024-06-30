70bf901fd0f013da0df482d5c88918ac5e795de4
Wolfgang Barth <wob@swobspace.net>
2024-06-30 11:30:48 +0200

    set condition_message with initial check

---
+++ db/migrate/20240630092451_update_condition_message.rb
@@ -0,0 +1,15 @@
+class UpdateConditionMessage < ActiveRecord::Migration[7.1]
+  #
+  # update condition message with current state as initial start value
+  #
+  def up
+    (Connector.all + CardTerminal.all + Card.all).each do |c|
+      c.update_condition
+      c.save
+    end
+  end
+
+  def down
+    # do nothing
+  end
+end
