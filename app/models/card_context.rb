8e7e1bc426b32ca30d2422d69c7d581fbf05dd1b
Wolfgang Barth <wob@swobspace.net>
2024-07-13 11:58:27 +0200

    add inverse_of

--- app/models/card_context.rb
+++ app/models/card_context.rb
@@ -1,7 +1,7 @@
 class CardContext < ApplicationRecord
   # -- associations
-  belongs_to :card, optional: false
-  belongs_to :context, optional: false
+  belongs_to :card, optional: false, inverse_of: :card_contexts
+  belongs_to :context, optional: false, inverse_of: :card_contexts
 
   # -- configuration
   acts_as_list scope: :card
