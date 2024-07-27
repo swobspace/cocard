b6177575bec447c67779d31c149301551144ce15
Wolfgang Barth <wob@swobspace.net>
2024-07-27 17:40:49 +0200

    refine timestamp info: time distance

--- app/components/is_current_component.rb
+++ app/components/is_current_component.rb
@@ -1,25 +1,60 @@
 # frozen_string_literal: true
 
 class IsCurrentComponent < ViewComponent::Base
-  def initialize(item:, attr:, grace_period: Cocard::grace_period)
+  def initialize(item:, attr:, grace_period: Cocard::grace_period, relative: false, icon: true)
     @item = item
     @attr = attr
     @grace_period = grace_period
+    @relative = relative
+    @icon = icon
   end
 
   def message
-    to_check = item.send(attr) || ""
-    if to_check.blank? 
+    if !icon
+      show_time.to_s
+    elsif to_check.blank? 
       Cocard::States.flag(Cocard::States::WARNING)
     elsif to_check < grace_period.before(Time.current)
-      Cocard::States.flag(Cocard::States::WARNING) + " #{to_check.localtime}"
+      Cocard::States.flag(Cocard::States::WARNING) + " #{show_time}"
     else
-      Cocard::States.flag(Cocard::States::OK) + " #{to_check.localtime}"
+      Cocard::States.flag(Cocard::States::OK) + " #{show_time}"
     end
   end
 
 
 private
-  attr_reader :item, :grace_period, :attr
+  attr_reader :item, :grace_period, :attr, :relative, :icon
+
+  def to_check
+    @to_check ||= item.send(attr) || ""
+  end
+
+  def show_time
+    return "" if to_check.blank?
+    if relative
+      time_distance(to_check)
+    else
+      to_check.localtime
+    end
+  end
+
+  def time_distance(ts)
+    return "" if ts.blank?
+    tdist = Time.current - ts
+    case
+      when tdist >= 1.year
+        "#{(tdist/(12*30*24*3600)).round} year(s)"
+      when tdist >= 1.month
+        "#{(tdist/(30*24*3600)).round} month(s)"
+      when tdist >= 1.day
+        "#{(tdist/(24*3600)).round} day(s)"
+      when tdist >= 1.hour
+        "#{(tdist/3600).round} h"
+      when tdist >= 1.minute
+        "#{(tdist/60).round} min"
+      else
+        "aktuell"
+    end
+  end
 
 end
