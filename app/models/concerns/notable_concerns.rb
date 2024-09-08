61bad8b7949ab743deca615e7620e51c03a35dd1
Wolfgang Barth <wob@swobspace.net>
2024-09-08 13:25:05 +0200

    working on acknowledges, not finished

--- app/models/concerns/notable_concerns.rb
+++ app/models/concerns/notable_concerns.rb
@@ -12,14 +12,14 @@ module NotableConcerns
 
   def current_note
     notes
-    .where("notes.valid_until <= ? or notes.valid_until IS NULL", Date.current)
+    .where("notes.valid_until >= ? or notes.valid_until IS NULL", Date.current)
     .order("valid_until DESC NULLS FIRST, id DESC")
     .first
   end
   
   def current_acknowledge
     acknowledges
-    .where("notes.valid_until <= ? or notes.valid_until IS NULL", Date.current)
+    .where("notes.valid_until >= ? or notes.valid_until IS NULL", Date.current)
     .order("valid_until DESC NULLS FIRST, id DESC")
     .first
   end
