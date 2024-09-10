2524d8c9acfe2fde3d3a2ba65ef666fd59d4d8ec
Wolfgang Barth <wob@swobspace.net>
2024-09-10 11:48:36 +0200

    create wrapper horizontal_datetime

--- config/initializers/simple_form_bootstrap_wob.rb
+++ config/initializers/simple_form_bootstrap_wob.rb
@@ -15,6 +15,22 @@ SimpleForm.setup do |config|
     end
   end
 
+  config.wrappers :horizontal_datetime, tag: 'div', class: 'mb-3 row', error_class: 'form-group-invalid', valid_class: 'form-group-valid' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :pattern
+    b.optional :readonly
+    b.use :label, class: 'col-sm-3 form-control-label'
+
+    b.wrapper :grid_wrapper, tag: 'div', class: 'col-sm-4' do |ba|
+      ba.use :input, class: 'form-control', type: 'datetime-local', autocomplete: 'off', error_class: 'is-invalid', valid_class: 'is-valid'
+
+      ba.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+      ba.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
+
+    end
+  end
+
   config.wrappers :horizontal_select, tag: 'div', class: 'mb-3 row', error_class: 'form-group-invalid', valid_class: 'form-group-valid' do |b|
     b.use :html5
     b.use :placeholder
