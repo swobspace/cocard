72957d6e1d09098a1e1df54b77ff1d1766bb9b76
Wolfgang Barth <wob@swobspace.net>
2024-09-14 17:56:13 +0200

    add ENV for card terminal rmi interface

--- app/services/card_terminals/rmi/base.rb
+++ app/services/card_terminals/rmi/base.rb
@@ -15,17 +15,19 @@ module CardTerminals
       def initialize(options = {})
         options.symbolize_keys
         @card_terminal = options.fetch(:card_terminal)
-        @valid, @rmi = check_terminal
         @messages = []
+        @valid, @rmi = check_terminal
       end
 
     private
 
       def check_terminal
-        if card_terminal.product_information&.product_code == "ORGA6100" &&
-           card_terminal.firmware_version >= '3.9.0'
+        pc = card_terminal.product_information&.product_code
+        fw = card_terminal.firmware_version
+        if pc == "ORGA6100" && fw >= '3.9.0'
            [true, CardTerminals::RMI::OrgaV1]
         else
+           @messages << "CardTerminal #{pc} with firmware #{fw} is not supported"
            [false, nil]
         end
       end
