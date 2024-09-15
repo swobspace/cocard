1b10d5f8c1591ba512196638ae85bd487421941c
Wolfgang Barth <wob@swobspace.net>
2024-09-15 11:58:40 +0200

    bundle update && yarn upgrade

--- Gemfile
+++ Gemfile
@@ -98,7 +98,7 @@ gem "immutable_struct", "~> 1.2"
 
 gem "net-ping", "~> 2.0"
 
-gem "good_job", "~> 4.1.0"
+gem "good_job", "~> 4.3.0"
 
 gem "acts_as_list", "~> 1.1"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_14_155737) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_15_093613) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -226,6 +226,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_14_155737) do
     t.datetime "enqueued_at"
     t.datetime "discarded_at"
     t.datetime "finished_at"
+    t.datetime "jobs_finished_at"
   end
 
   create_table "good_job_executions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
