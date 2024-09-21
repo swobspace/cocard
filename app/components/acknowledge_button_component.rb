d731f7516308319da263f177456cdb2ebcdfbc05
Wolfgang Barth <wob@swobspace.net>
2024-09-21 11:10:22 +0200

    styling acknowledge button

--- app/components/acknowledge_button_component.rb
+++ app/components/acknowledge_button_component.rb
@@ -20,7 +20,7 @@ class AcknowledgeButtonComponent < ViewComponent::Base
 
   def button_css
     if @current.nil?
-      "btn #{btn_size} btn-warning"
+      "btn #{btn_size} btn-outline-warning text-acknowledge"
     else
       "btn #{btn_size} btn-outline-secondary"
     end
