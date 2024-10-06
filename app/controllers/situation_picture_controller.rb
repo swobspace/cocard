af14c9959a3846d0c4712a69d95de92a3a4a9b00
Wolfgang Barth <wob@swobspace.net>
2024-10-06 17:17:50 +0200

    catch error condition

--- app/controllers/situation_picture_controller.rb
+++ app/controllers/situation_picture_controller.rb
@@ -3,6 +3,10 @@ class SituationPictureController < ApplicationController
     result = ::TI::SituationPicture::Fetch.new().call
     if result.success?
       @situation_picture = result.situation_picture
+    else
+      @situation_picture = []
+      flash[:alert] = "ERROR: TI Lagebild kann nicht abgefragt werden: " +
+                      result.error_messages.join('; ')
     end
   end
 end
