49fbf73d2e2875e6f73cc6ee827baa67f0a514fe
Wolfgang Barth <wob@swobspace.net>
2024-11-01 17:42:32 +0100

    add availability switch

--- app/controllers/situation_picture_controller.rb
+++ app/controllers/situation_picture_controller.rb
@@ -1,5 +1,10 @@
 class SituationPictureController < ApplicationController
   def index
+    if params[:availability]
+      @situation_picture = SinglePicture.availability(params[:availability])
+    else
+      @situation_picture = SinglePicture.all
+    end
     respond_with(@situation_picture)
   end
 end
