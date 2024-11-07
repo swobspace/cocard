7505c6ce8bd4ecaa690d39cd179c37a2cb653374
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2024-11-07 09:27:17 +0100

    ti lagebild grouped by tid

--- app/controllers/situation_picture_controller.rb
+++ app/controllers/situation_picture_controller.rb
@@ -2,9 +2,17 @@ class SituationPictureController < ApplicationController
   def index
     if params[:availability]
       @situation_picture = SinglePicture.availability(params[:availability])
+    elsif params[:with_failed_tids]
+      @situation_picture = SinglePicture.with_failed_tids
     else
       @situation_picture = SinglePicture.all
     end
     respond_with(@situation_picture)
+  end
+
+  def failed
+    @situation_picture = SinglePicture.with_failed_tids
+    @tids = @situation_picture.select(:tid, :availability)
+                              .group(:tid).maximum("availability")
   end
 end
