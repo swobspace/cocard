39a25b3983dd9ce3cbdde9a25bda852b7430965f
Wolfgang Barth <wob@swobspace.net>
2024-11-08 16:39:54 +0100

    fix spec

--- spec/components/ti/lagebild_alert_button_component_spec.rb
+++ spec/components/ti/lagebild_alert_button_component_spec.rb
@@ -18,14 +18,14 @@ RSpec.describe TI::LagebildAlertButtonComponent, type: :component do
     sp = FactoryBot.create(:single_picture, availability: 0, tid: "tid-13")
     render_inline(described_class.new())
     # puts rendered_content
-    expect(page).to have_css('a[class="btn btn-warning mb-0"]')
+    expect(page).to have_css('a[class="btn btn-warning text-white mb-0"]')
     expect(page).to have_css(%Q[a[href="#{failed_situation_picture_path()}"]])
   end
 
   it "don't show button" do
     sp = FactoryBot.create(:single_picture, availability: 1, tid: "tid-13")
-    # render_inline(described_class.new())
-    puts rendered_content
+    render_inline(described_class.new())
+    # puts rendered_content
     expect(page).not_to have_css('a[class="btn btn-warning mb-0"]')
     expect(page).not_to have_css('a[class="btn btn-danger mb-0"]')
     expect(page).not_to have_css(%Q[a[href="#{failed_situation_picture_path()}"]])
