fb877b43afd45f42599afd904ad277f45b026867
Wolfgang Barth <wob@swobspace.net>
2024-11-30 10:39:11 +0100

    log client_certificate params in development

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,6 +3,12 @@
 # Configure parameters to be partially matched (e.g. passw matches password) and filtered from the log file.
 # Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
-Rails.application.config.filter_parameters += [
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
-]
+if Rails.env.development?
+  Rails.application.config.filter_parameters += [
+    :passw, :secret, :token, :_key, :crypt, :salt, :otp, :ssn, :passphrase
+  ]
+else
+  Rails.application.config.filter_parameters += [
+    :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :passphrase
+  ]
+end
