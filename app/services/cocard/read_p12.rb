41343e636dfead1c7dc895e7b2e5b23a50dda967
Wolfgang Barth <wob@swobspace.net>
2024-11-30 12:20:48 +0100

    p12_to_params as service: retry with cmdline and openssl -legacy

--- app/services/cocard/read_p12.rb
+++ app/services/cocard/read_p12.rb
@@ -17,7 +17,7 @@ module Cocard
     #
     def initialize(options = {})
       options.symbolize_keys
-      @p12      = options.fetch(:p12)
+      @p12      = String(options.fetch(:p12))
       @exportpw = options.fetch(:exportpw)
     end
 
@@ -25,34 +25,63 @@ module Cocard
     # do all the work here ;-)
     def call
       error_messages = []
+
       if p12.blank?
-        error_messages << "No P12-File specified"
+        error_messages << "No p12 file specified"
+      elsif !File.readable?(p12)
+        error_messages << "p12 file is not readable"
       end
+
       if error_messages.any?
         return Result.new(success?: false, error_messages: error_messages,
                           params: {})
       end
 
       begin
+        # 
+        # try Ruby OpenSSL::PKCS12 first
+        #
         pkcs12 = OpenSSL::PKCS12.new(File.read(p12), exportpw)
         cert = pkcs12.certificate.to_pem
         unsecure_key = pkcs12.key
         cipher = OpenSSL::Cipher.new 'aes-256-cbc'
-        passphrase = SecureRandom.base64(42)
         pkey = unsecure_key.private_to_pem cipher, passphrase
 
         Result.new(success?: true, error_messages: error_messages, 
                    params: { cert: cert, pkey: pkey, passphrase: passphrase })
 
       rescue OpenSSL::PKCS12::PKCS12Error
-        error_messages << "Could not parse P12 file"
-        Result.new(success?: false, error_messages: error_messages, 
-                   params: {})
+        #
+        # if OpenSSL::PKCS12 fails, try openssl from cmdline with -legacy
+        #
+        cert = cert_from_p12(p12, exportpw)
+        pkey = pkey_from_p12(p12, exportpw, passphrase)
+
+        if cert.present? and pkey.present?
+          Result.new(success?: true, error_messages: error_messages, 
+                     params: { cert: cert, pkey: pkey, passphrase: passphrase })
+        else 
+          error_messages << "Could not parse P12 file"
+          Result.new(success?: false, error_messages: error_messages, 
+                     params: {})
+        end
       end
     end
 
   private
     attr_reader :p12, :exportpw
+
+    def passphrase
+      @passphrase ||= SecureRandom.base64(42)
+    end
+
+    def cert_from_p12(p12, exportpw)
+      cert = `openssl pkcs12 -in #{p12} -clcerts -nokeys -passin pass:#{exportpw} -legacy`
+    end
+
+    def pkey_from_p12(p12, exportpw, passphrase)
+      pkey = `openssl pkcs12 -in #{p12} -nocerts -passin pass:#{exportpw} -passout pass:#{passphrase} -legacy`
+    end
 
   end
 end
--- spec/services/cocard/read_p12_spec.rb
+++ spec/services/cocard/read_p12_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 module Cocard
   RSpec.describe ReadP12 do
     let(:p12_file) do
-      file = File.join(Rails.root, 'spec', 'fixtures', 'files', 
-                       'demo.p12')
+      file = File.join(Rails.root, 'spec', 'fixtures', 'files', 'demo.p12')
     end
     let(:exportpw) { 'justfortesting' }
 
@@ -22,8 +21,40 @@ module Cocard
       it { expect(subject.respond_to?(:call)).to be_truthy }
     end
 
-    describe "with new p12 format" do
+    describe "with missing p12 file" do
+      let(:p12_file) { nil }
+      it { expect(subject.call.success?).to be_falsey }
+      it { expect(subject.call.error_messages).to include("No p12 file specified") }
+    end
 
+    describe "with nonexistent p12 file" do
+      let(:p12_file) do
+        file = File.join(Rails.root, 'spec', 'fixtures', 'files', 'doesnotexist')
+      end
+      it { expect(subject.call.success?).to be_falsey }
+      it { expect(subject.call.error_messages).to include("p12 file is not readable") }
+    end
+
+    describe "with new p12 format" do
+      describe '#call' do
+        describe "successful call" do
+          it { expect(subject.call.success?).to be_truthy }
+          it { expect(subject.call.params).to be_kind_of(Hash) }
+
+          describe 'get some information' do
+            let(:params) { subject.call.params }
+            it { expect(params[:cert]).to match(/BEGIN CERTIFICATE/) }
+            it { expect(params[:pkey]).to match(/BEGIN ENCRYPTED PRIVATE KEY/) }
+            it { expect(params[:passphrase].length).to eq(56) }
+          end
+        end
+      end
+    end
+
+    describe "with legacy p12 format" do
+      let(:p12_file) do
+        file = File.join(Rails.root, 'spec', 'fixtures', 'files', 'demo-legacy.p12')
+      end
       describe '#call' do
         describe "successful call" do
           it { expect(subject.call.success?).to be_truthy }
