6b9826c4e502e6a5247b35a76ae62c9f9b2b32aa
Wolfgang Barth <wob@swobspace.net>
2025-03-01 16:59:50 +0100

    more logging

--- app/jobs/card_terminals/rmi/get_idle_message_job.rb
+++ app/jobs/card_terminals/rmi/get_idle_message_job.rb
@@ -13,8 +13,10 @@ module CardTerminals
 
         if card_terminal.is_a? Array
           card_terminal.each do |ct|
-            # create one job for each card terminal
-            CardTerminals::RMI::GetIdleMessageJob.perform_later(card_terminal: ct)
+            if ct.condition == Cocard::States::OK and ct.supports_rmi?
+              # create one job for each card terminal
+              CardTerminals::RMI::GetIdleMessageJob.perform_later(card_terminal: ct)
+            end
           end
         else
           @prefix = "GetIdleMessage:: card_terminal #{card_terminal}:: ".freeze
@@ -23,6 +25,14 @@ module CardTerminals
             _rmi.get_idle_message
             if _rmi.result['idle_message']
               card_terminal.update(idle_message: _rmi.result['idle_message'])
+              Rails.logger.info("INFO:: #{card_terminal} - " +
+                                "idle_message == #{card_terminal.idle_message}")
+
+              return true
+            else
+              Rails.logger.warn("WARN:: #{card_terminal} - " +
+                                "could not get idle_message")
+              return false
             end
           end
         end
@@ -34,6 +44,10 @@ module CardTerminals
         rmi = CardTerminals::RMI::Base.new(card_terminal: card_terminal)
         if !rmi.valid
           Rails.logger.warn(prefix + "CardTerminal does not meet requirements" + 
+                            rmi.messages.join(', '))
+          false
+        elsif card_terminal.condition != Cocard::States::OK
+          Rails.logger.warn(prefix + "CardTerminal condition must be OK, skipping" +
                             rmi.messages.join(', '))
           false
         else
