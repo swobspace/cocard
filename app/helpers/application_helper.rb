4437c0261347c384ca470cf4f9161ae2101bdbe4
Wolfgang Barth <wob@swobspace.net>
2025-05-30 14:58:25 +0200

    styling tag list input

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,12 +9,24 @@ module ApplicationHelper
   end
 
   def tag_list_input(form)
-    form.input :tag_list_input,
-               input_html: {
-                 value: form.object.tag_list.to_json,
-                 class: "form-select",
-                 data: { controller: "tagging",
-                         "tagging-options-value": Tag.all.map(&:name)} }
+    element = <<~EOFELEMENT
+      <div class="row mb-3 string optional">
+        #{form.label :tag_list_input,
+                     class: "col-sm-3 col-form-label string optional"}
+        <div class="col-sm-9">
+          #{form.text_field :tag_list_input,
+                            class:"string optional w-100 rounded-2",
+                            value: form.object.tag_list.to_json,
+                            data: { 
+                              controller: "tagging",
+                              "tagging-options-value": Tag.all.map(&:name)
+                            }
+           }
+        </div>
+      </div>
+    EOFELEMENT
+
+    element.html_safe
   end
 
 end
