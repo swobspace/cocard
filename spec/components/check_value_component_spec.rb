5404060ca593ec324a3c589e0ef8ae22d957f1a6
Wolfgang Barth <wob@swobspace.net>
2025-06-28 16:12:31 +0200

    spec for check_value_component

--- spec/components/check_value_component_spec.rb
+++ spec/components/check_value_component_spec.rb
@@ -2,14 +2,66 @@
 
 require "rails_helper"
 
+# class CheckValueComponent < ViewComponent::Base
+#   def initialize(value:, condition:, message:, icon: true, level: :info)
+#
 RSpec.describe CheckValueComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "with condition false" do
+    it "shows no message, no bg color" do
+      render_inline(described_class.new(value: '99.7.2', 
+                                        condition: false,
+                                        message: 'MyMessage',
+                                        icon: true,
+                                        level: :info))
+      # puts rendered_content
+      expect(page).to have_content("✅ 99.7.2")
+      expect(page).to have_content("MyMessage")
+      expect(page).to have_css('span[class=""]')
+    end
+  end
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  describe "with condition true" do
+    it "shows message and bg color bg-info" do
+      render_inline(described_class.new(value: '99.7.2', 
+                                        condition: true,
+                                        message: 'MyMessage',
+                                        icon: true,
+                                        level: :info))
+      # puts rendered_content
+      expect(page).to have_content("⚠ 99.7.2")
+      expect(page).to have_content("MyMessage")
+      expect(page).to have_css('span[class="badge text-bg-info opacity-75"]')
+    end
+  end
+
+  describe "with condition true and level :danger" do
+    it "shows message and bg color bg-danger" do
+      render_inline(described_class.new(value: '99.7.2', 
+                                        condition: true,
+                                        message: 'MyMessage',
+                                        icon: true,
+                                        level: :critical))
+      # puts rendered_content
+      expect(page).to have_content("⚠ 99.7.2")
+      expect(page).to have_content("MyMessage")
+      expect(page).to have_css('span[class="badge text-bg-danger opacity-75"]')
+    end
+  end
+
+  describe "with icon false" do
+    it "shows no icon" do
+      render_inline(described_class.new(value: '99.7.2', 
+                                        condition: false,
+                                        message: 'MyMessage',
+                                        icon: false,
+                                        level: :info))
+      # puts rendered_content
+      expect(page).not_to have_content("✅ 99.7.2")
+      expect(page).to have_content("99.7.2")
+      expect(page).to have_content("MyMessage")
+      expect(page).to have_css('span[class=""]')
+    end
+  end
+
+
 end
