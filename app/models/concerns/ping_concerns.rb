24a079f2c5e942ccd2d55d6583c1f5f85d3b585b
Wolfgang Barth <wob@swobspace.net>
2025-07-06 11:13:48 +0200

    performance tuning: caching ping

--- app/models/concerns/ping_concerns.rb
+++ app/models/concerns/ping_concerns.rb
@@ -10,7 +10,9 @@ module PingConcerns
     return false if noip?
     # check = Net::Ping::External.new(ip.to_s, nil, 2)
     # check.ping?
-    system("fping -a -q -p 100 -r 5 #{ip} >/dev/null")
+    Rails.cache.fetch("#{cache_key_with_version}/up", expires_in: 1.minute) do
+      system("fping -a -q -p 100 -r 5 -B 1 #{ip} >/dev/null")
+    end
   end
 
   def noip?
