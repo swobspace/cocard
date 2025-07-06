ab8f81644fdd741e6b0e9040cc925437f01dab9d
Wolfgang Barth <wob@swobspace.net>
2025-07-06 10:02:43 +0200

    fping: use -B default

--- app/models/concerns/ping_concerns.rb
+++ app/models/concerns/ping_concerns.rb
@@ -10,7 +10,7 @@ module PingConcerns
     return false if noip?
     # check = Net::Ping::External.new(ip.to_s, nil, 2)
     # check.ping?
-    system("fping -a -q -p 100 -r 5 -B 1.25 #{ip} >/dev/null")
+    system("fping -a -q -p 100 -r 5 #{ip} >/dev/null")
   end
 
   def noip?
