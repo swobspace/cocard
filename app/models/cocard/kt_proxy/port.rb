4bfd200751efb35a7614807380b6f1a10be8adac
Wolfgang Barth <wob@swobspace.net>
2025-09-07 10:58:10 +0200

    model for port selection

---
+++ app/models/cocard/kt_proxy/port.rb
@@ -0,0 +1,31 @@
+module Cocard
+  module KTProxy
+    class Port
+      attr_reader :port_range, :min_port, :max_port, :used_ports
+
+      def initialize(port_range:, used_ports:)
+        @port_range = port_range
+        @used_ports = used_ports
+        (@min_port, @max_port) = port_range.split(/:/).map(&:to_i)
+      end
+
+      def next_port
+        mport = @used_ports.max
+        if mport < max_port
+          mport += 1
+        else
+          first_unused_port
+        end
+      end
+
+      def unused_ports
+        # (8..16).select{|x| ![1,9,17,16,4].include?(x)}
+        (min_port..max_port).select{|x| !used_ports.include?(x)}.sort
+      end
+
+      def first_unused_port
+        unused_ports.first || -1
+      end
+    end
+  end
+end
