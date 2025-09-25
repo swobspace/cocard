f0f4eeaf72de47a2115cfb6734b3d20e6a261e07
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2025-09-25 14:32:14 +0200

    lessen restriction on remote pairing button

--- app/components/card_terminal/pairing_button_component.rb
+++ app/components/card_terminal/pairing_button_component.rb
@@ -6,22 +6,41 @@ class CardTerminal::PairingButtonComponent < ViewComponent::Base
   end
 
   def title
-    I18n.t('card_terminals.remote_pairing')
+    if supports_pairing?
+      I18n.t('card_terminals.remote_pairing')
+    else
+      I18n.t('card_terminals.remote_pairing') + " wird ggf. nicht unterstützt"
+    end
+  end
+
+  def button_class
+    if supports_pairing?
+      "btn btn-warning me-1"
+    else
+      "btn btn-outline-warning me-1"
+    end
+
   end
 
   def confirmation
-    # I18n.t('card_terminals.do_remote_pairing') + '?'
-    I18n.t('card_terminals.do_remote_pairing') + '?' +
-    " - danach bitte Pairingvorgang am Konnektor manuell starten"
+    if supports_pairing?
+      I18n.t('card_terminals.do_remote_pairing') + '?' +
+      " - danach bitte Pairingvorgang am Konnektor manuell starten"
+    else
+      I18n.t('card_terminals.do_remote_pairing') + '?' +
+      " !Remote-Pairing wird vom Terminal ggf. nicht unterstützt!"
+    end
   end
 
   def render?
     !card_terminal.connected &&
-    card_terminal.supports_rmi? && 
-    card_terminal.rmi.available_actions.include?(:remote_pairing)
+    card_terminal.supports_rmi? 
   end
 
 private
   attr_reader :card_terminal
 
+  def supports_pairing?
+    card_terminal.rmi.available_actions.include?(:remote_pairing)
+  end
 end
