4a00a090bf2d9d6e4b10c52287916b7929b850fc
Wolfgang Barth <wob@swobspace.net>
2025-10-05 17:49:21 +0200

    fix spec

--- spec/components/warn_ip_mismatch_component_spec.rb
+++ spec/components/warn_ip_mismatch_component_spec.rb
@@ -3,19 +3,26 @@
 require "rails_helper"
 
 RSpec.describe WarnIpMismatchComponent, type: :component do
+  let(:conn)          { FactoryBot.create(:connector) }
   let(:card_terminal) { FactoryBot.create(:card_terminal, :with_mac) }
 
   describe "without mismatch" do
     it "shows no badge at all" do
+      allow(card_terminal).to receive(:ip).and_return("2.3.4.5")
+      allow(card_terminal).to receive(:current_ip).and_return("nil")
       render_inline(described_class.new(item: card_terminal))
+      puts rendered_content
       expect(page).not_to have_css('span')
     end
   end
 
   describe "with ip mismatch" do
     it "shows warning badge" do
+      allow(card_terminal).to receive(:ip).and_return("2.3.4.5")
       allow(card_terminal).to receive(:current_ip).and_return("1.2.3.4")
+      allow(card_terminal).to receive(:connector).and_return(conn)
       render_inline(described_class.new(item: card_terminal))
+      # puts rendered_content
       expect(page).to have_css('span[class="badge text-bg-info opacity-75"]')
     end
   end
