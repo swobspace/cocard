24795c54d6e9927b0330a64637cddc9333859cda
Wolfgang Barth <wob@swobspace.net>
2025-10-05 17:31:19 +0200

    complete duck_terminal controller and view

--- app/controllers/duck_terminals_controller.rb
+++ app/controllers/duck_terminals_controller.rb
@@ -12,13 +12,15 @@ class DuckTerminalsController < ApplicationController
     rmi = CardTerminals::RMI.new(card_terminal: @ducky)
     rmi.get_info do |result|
       result.on_failure do |message|
-        errormsg = "Abfrage des Kartenterminals fehlgeschlagen! #{message}"
+        @message = message
+        errormsg = "Abfrage des Kartenterminals fehlgeschlagen!"
         Rails.logger.debug("DEBUG:: get_info: #{errormsg}")
         flash[:alert] = errormsg
       end
 
       result.on_success do |message, value|
         @value = value
+        @message = message
         @card_terminal = CardTerminal.find_or_create_by(mac: value.macaddr) do |c|
                            c.ip = value.current_ip
                            # c.current_ip = value.current_ip
@@ -29,6 +31,7 @@ class DuckTerminalsController < ApplicationController
       end
 
       result.on_unsupported do
+        @message = 'unsupported'
         flash[:alert] = "Kartenterminal wird nicht unterstützt"
       end
     end
--- spec/requests/duck_terminal_spec.rb
+++ spec/requests/duck_terminal_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "DuckTerminals", type: :request do
+  before(:each) do
+    login_admin
+  end
+
   describe "GET /new" do
     it "returns http success" do
       get "/duck_terminal/new"
@@ -8,10 +12,63 @@ RSpec.describe "DuckTerminals", type: :request do
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/duck_terminal/create"
-      expect(response).to have_http_status(:success)
+  describe "with rmi port unreachable" do
+    let(:new_params) {{
+      duck_terminal: {
+        ip: '127.0.0.4',
+        identification: 'INGHC-ORGA6100',
+        firmware_version: '3.9.0'
+      }
+    }}
+
+    describe "GET #show" do
+      it "returns http success" do
+        get duck_terminal_url, params: new_params
+        expect(response).to have_http_status(:success)
+        # puts response.body
+        expect(response.body).to match('Abfrage des Kartenterminals fehlgeschlagen')
+        expect(response.body).to match('RMI-Port 443 unreachable!')
+      end
+    end
+  end
+
+  describe "with unsupported firmware version" do
+    let(:new_params) {{
+      duck_terminal: {
+        ip: '127.0.0.4',
+        identification: 'INGHC-ORGA6100',
+        firmware_version: '1.1.1'
+      }
+    }}
+
+    describe "GET #show" do
+      it "returns http success" do
+        get duck_terminal_url, params: new_params
+        expect(response).to have_http_status(:success)
+        # puts response.body
+        expect(response.body).to match('Kartenterminal wird nicht unterstützt')
+        expect(response.body).to match('unsupported')
+      end
+    end
+  end
+
+  describe "with supported terminal", rmi: true do
+    let(:new_params) {{
+      duck_terminal: {
+        ip: ENV['CT_IP'],
+        identification: 'INGHC-ORGA6100',
+        firmware_version: '3.9.0'
+      }
+    }}
+
+    describe "GET #show" do
+      it "returns http success" do
+        get duck_terminal_url, params: new_params
+        expect(response).to have_http_status(:success)
+        puts response.body
+        expect(response.body).to match('000DF80C8652')
+        expect(response.body).to match('ORGA6100-0241000000B692')
+      end
     end
   end
 
