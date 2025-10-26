2ad87d1444c59689ae4c792bc48268c9abe2a35a
Wolfgang Barth <wob@swobspace.net>
2025-10-26 16:44:38 +0100

    string only!

--- app/models/concerns/kt_proxy_concerns.rb
+++ app/models/concerns/kt_proxy_concerns.rb
@@ -8,13 +8,13 @@ module KTProxyConcerns
     Jbuilder.new do |kt|
       kt.set! 'id', uuid
       kt.set! 'name', name
-      kt.set! 'wireguardIp', wireguard_ip
-      kt.set! 'incomingIp', incoming_ip
-      kt.set! 'incomingPort', incoming_port
-      kt.set! 'outgoingIp', outgoing_ip
-      kt.set! 'outgoingPort', outgoing_port
-      kt.set! 'cardTerminalIp', card_terminal_ip
-      kt.set! 'cardTerminalPort', card_terminal_port
+      kt.set! 'wireguardIp', wireguard_ip.to_s
+      kt.set! 'incomingIp', incoming_ip.to_s
+      kt.set! 'incomingPort', incoming_port.to_s
+      kt.set! 'outgoingIp', outgoing_ip.to_s
+      kt.set! 'outgoingPort', outgoing_port.to_s
+      kt.set! 'cardTerminalIp', card_terminal_ip.to_s
+      kt.set! 'cardTerminalPort', card_terminal_port.to_s
     end
   end
 
