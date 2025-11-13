800a527d23b0c9589c0d957777c5b7f368bc635a
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2025-11-13 10:10:06 +0100

    fix blocking migration

--- db/migrate/20251003075413_preset_identification.rb
+++ db/migrate/20251003075413_preset_identification.rb
@@ -1,6 +1,6 @@
 class PresetIdentification < ActiveRecord::Migration[7.2]
   def up
-    CardTerminal.where.not(properties: nil).each do |ct|
+    CardTerminal.where.not(properties: nil).where.not(identification: "").each do |ct|
       identification = "#{ct.product_information&.product_vendor_id}-#{ct.product_information&.product_code}"
       ct.update(identification: identification)
     end
