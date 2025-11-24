68c0166e7995cb11226f237b3a852fb5782c88d3
Wolfgang Barth <wob@swobspace.net>
2025-11-24 10:01:28 +0100

    ldap search with client cert working

--- utils/ldap_with_client_cert.rb
+++ utils/ldap_with_client_cert.rb
@@ -2,17 +2,20 @@
 # frozen_string_literal: true
 
 
-ENV['RAILS_ENV'] ||= 'test'
+ENV['RAILS_ENV'] ||= 'development'
 require "#{File.dirname(__FILE__)}/../config/environment.rb"
 
 connector = Connector.where("name ILIKE ?", '%01-Spielwiese').first
 client_certificate = ClientCertificate.where(client_system: 'cocard').first
 
+cert_store = OpenSSL::X509::Store.new
+cert_store.add_file "./utils/cacert.pem"
+
 tls_options = {
   cert: client_certificate.certificate,
   key: client_certificate.private_key,
-  ca_path: '/etc/pki/ca-trust/extracted/openssl',
-  verify_mode: OpenSSL::SSL::VERIFY_NONE,
+  cert_store: cert_store,
+  verify_mode: (OpenSSL::SSL::VERIFY_PEER | OpenSSL::SSL::VERIFY_FAIL_IF_NO_PEER_CERT),
   verify_hostname: false
 }
 
@@ -20,17 +23,20 @@ ldap_options = {
   host: connector.ip.to_s,
   port: 636,
   base: "dc=data,dc=vzd",
-  encryption: :simple_tls,
-  tls_options: tls_options
+  encryption: {
+    method: :simple_tls,
+    tls_options: tls_options
+  }
 }
 
 ldap = Wobaduser::LDAP.new(ldap_options: ldap_options)
 
 search = Wobaduser::User.search(ldap: ldap, 
                                 filter: '(mail=chirurgie.sls@marienhaus.kim.telematik)')
-search.success?
-search.entries.count
-search.entries
+puts search.success?
+puts search.entries.count
+
 entry = search.entries.first
+puts entry.inspect
 
 
