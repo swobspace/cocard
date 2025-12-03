05fa51da03d104c83349f35cb4e90fa335f0540a
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2025-12-03 15:02:37 +0100

    get info single filed

--- app/jobs/card_terminals/rmi/get_info_job.rb
+++ app/jobs/card_terminals/rmi/get_info_job.rb
@@ -9,58 +9,55 @@ module CardTerminals
       #
       def perform(options = {})
         options = options.symbolize_keys
-        card_terminal = options.fetch(:card_terminal) { CardTerminal.ok.to_a }
+        card_terminals = options.fetch(:card_terminal) { CardTerminal.ok.to_a }
+        card_terminals = Array(card_terminals)
 
-        if card_terminal.is_a? Array
-          card_terminal.each do |ct|
-            if ct.condition == Cocard::States::OK and ct.supports_rmi?
-              # create one job for each card terminal
-              CardTerminals::RMI::GetInfoJob.perform_later(card_terminal: ct)
+        if ct.condition == Cocard::States::OK and ct.supports_rmi?
+          card_terminals.each do |card_terminal|
+            # slow down
+            sleep 3
+            @prefix = "GetInfo:: card_terminal #{card_terminal}:: ".freeze
+            unless check_job_requirements(card_terminal)
+              Rails.logger.debug(@prefix + "not all requirements met")
+              return false
             end
-          end
-        else
-          # single card terminal
-          @prefix = "GetInfo:: card_terminal #{card_terminal}:: ".freeze
-          unless check_job_requirements(card_terminal)
-            Rails.logger.debug(@prefix + "not all requirements met")
-            return false
-          end
 
-          card_terminal.rmi.get_info do |result|
-            result.on_success do |message, info|
-              creator = CardTerminals::RMI::Creator.new(info: info)
-              if creator.save
-                Rails.logger.debug(@prefix + "Update des Kartenterminals erfolgreich")
-              else
-                Rails.logger.warn(@prefix + "Update des Kartenterminals fehlgeschlagen")
+            card_terminal.rmi.get_info do |result|
+              result.on_success do |message, info|
+                creator = CardTerminals::RMI::Creator.new(info: info)
+                if creator.save
+                  Rails.logger.debug(@prefix + "Update des Kartenterminals erfolgreich")
+                else
+                  Rails.logger.warn(@prefix + "Update des Kartenterminals fehlgeschlagen")
+                end
+                return true
               end
-              return true
-            end
 
-            result.on_failure do |message|
-              Rails.logger.error(@prefix + "RMI-Abfrage fehlgeschlagen: #{message}")
-              return false
-            end
+              result.on_failure do |message|
+                Rails.logger.error(@prefix + "RMI-Abfrage fehlgeschlagen: #{message}")
+                return false
+              end
 
-            result.on_unsupported do
-              Rails.logger.warn(@prefix + "Terminal or action not supported")
-              return false
+              result.on_unsupported do
+                Rails.logger.warn(@prefix + "Terminal or action not supported")
+                return false
+              end
             end
           end
         end
       end
 
-    private
-      attr_reader :prefix
-      def check_job_requirements(card_terminal)
-        if !card_terminal.supports_rmi?
-          false
-        elsif !card_terminal.rmi.available_actions.include?(:get_info)
-          false
-        else
-          true
+      private
+        attr_reader :prefix
+        def check_job_requirements(card_terminal)
+          if !card_terminal.supports_rmi?
+            false
+          elsif !card_terminal.rmi.available_actions.include?(:get_info)
+            false
+          else
+            true
+          end
         end
       end
     end
   end
-end
