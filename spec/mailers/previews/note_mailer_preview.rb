0dfae9f13f183ec9a5bf2c991f2edfb87c518f2c
Wolfgang Barth <wob@swobspace.net>
2025-12-25 09:29:24 +0100

    update spec to match new user param

--- spec/mailers/previews/note_mailer_preview.rb
+++ spec/mailers/previews/note_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/note_mailer
 class NoteMailerPreview < ActionMailer::Preview
   def send_note
-    NoteMailer.with(note: Note.last).send_note
+    NoteMailer.with(note: Note.last, user: Wobauth::User.first).send_note
   end
 end
