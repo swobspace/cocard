0969773cfe3488069dd2b436e839880c5ff1d807
Wolfgang Barth <wob@swobspace.net>
2026-01-10 12:21:40 +0100

    terminal specific soap call for get_cards

--- app/services/cocard/soap/get_cards.rb
+++ app/services/cocard/soap/get_cards.rb
@@ -4,8 +4,40 @@ module Cocard::SOAP
       :get_cards
     end
 
+    def fetch_specific_options(options)
+      @mandant       = options.fetch(:mandant)
+      @client_system = options.fetch(:client_system)
+      @workplace     = options.fetch(:workplace)
+      @ct_id         = options.fetch(:ct_id, nil)
+    end
+
+    def soap_message
+      {
+        "CCTX:Context" => {
+          "CONN:MandantId"      => @mandant,
+          "CONN:ClientSystemId" => @client_system,
+          "CONN:WorkplaceId"    => @workplace  }
+      }.merge(soap_addons)
+    end
+
+    def soap_addons
+      if @ct_id
+        { "CARDCMN:CtId" => @ct_id }
+      else
+        { }
+      end
+    end
+
     def soap_operation_attributes
-      { "mandant-wide": true }
+      { "mandant-wide": mandant_wide }
+    end
+
+    def mandant_wide
+      if @ct_id.present?
+        false
+      else
+        @mandant_wide
+      end
     end
   end
 end
