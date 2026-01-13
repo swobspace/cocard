7bba8983c22b8ca7b0622f186bd7d558d023f500
Wolfgang Barth <wob@swobspace.net>
2026-01-13 09:27:02 +0100

    option --crypt

--- utils/read_card_terminal_cards.rb
+++ utils/read_card_terminal_cards.rb
@@ -3,26 +3,20 @@
 
 ENV['RAILS_ENV'] ||= 'development'
 require "#{File.dirname(__FILE__)}/../config/environment.rb"
+require 'optparse'
 
-=begin
-options = OpenStruct.new
-options.plugin_ids = []
-options.hosts = []
+crypt = 'ECC'
 
 OptionParser.new do |opts|
-  opts.banner = "Usage: #{__FILE__} [options] <xmlfile> [... <xmlfile>]"
-  opts.on("-p", "--plugin-id <id>", "nessus plugin id to extract") do |pid|
-    options.plugin_ids += pid.split(/,/)
-  end
-  opts.on("-H", "--host <ip>", "extract only data from host") do |host|
-    options.hosts << host
+  opts.banner = "Usage: #{__FILE__} [options] card_terminal_id"
+  opts.on("-c", "--crypt RSA|ECC", "crypt algorithm") do |c|
+    crypt = c
   end
   opts.on("-h", "--help", "Prints this help") do
     puts opts
   end
 
 end.parse!
-=end
 
 if ARGV.empty?
   puts "Usage: #{__FILE__} card_terminal_id"
@@ -58,7 +52,7 @@ result.cards.each do |cc|
 end
 
 cards.each do |c|
-  printf "\n#{c.card_type}: #{c.to_s}"
+  printf "\n#{c.card_type}: #{c.to_s}\n"
   if c.card_type == 'SMC-B'
     cert_ref_list = %w( C.AUT C.ENC C.SIG )
   elsif c.card_type == 'HBA'
@@ -74,7 +68,7 @@ cards.each do |c|
              client_system: ctx&.client_system,
              workplace: ctx&.workplace,
              cert_ref_list: cert_ref_list,
-             crypt: 'RSA',
+             crypt: crypt,
              user_id: 'cocard'
            ).call
   unless result.success?
@@ -84,6 +78,8 @@ cards.each do |c|
   data_infos = Array(result.response.dig(:read_card_certificate_response, 
                                          :x509_data_info_list, :x509_data_info))
   data_infos.each do |di|
+    cert_ref = di.dig(:cert_ref)
+    puts "=== CertRef: #{cert_ref} ==="
     rawcert = di.dig(:x509_data, :x509_certificate)
     cert = Cocard::Certificate.new(rawcert)
     puts cert.cert.to_text
