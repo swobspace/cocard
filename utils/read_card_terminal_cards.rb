8dd78fb365cf46d2f804afb5ebc619b5072fc8ad
Wolfgang Barth <wob@swobspace.net>
2026-01-18 13:13:51 +0100

    using Cocard::CardCertificate

--- utils/read_card_terminal_cards.rb
+++ utils/read_card_terminal_cards.rb
@@ -78,11 +78,9 @@ cards.each do |c|
   data_infos = Array(result.response.dig(:read_card_certificate_response, 
                                          :x509_data_info_list, :x509_data_info))
   data_infos.each do |di|
-    cert_ref = di.dig(:cert_ref)
-    puts "=== CertRef: #{cert_ref} ==="
-    rawcert = di.dig(:x509_data, :x509_certificate)
-    cert = Cocard::Certificate.new(rawcert)
-    puts cert.cert.to_text
+    crt = Cocard::CardCertificate.new(di)
+    puts "=== CertRef: #{crt.cert_ref} ==="
+    puts crt.certificate.to_text
   end
   puts ""
 end
