15cb7535fd51f611e4b2c01dfbf752e08a83da2a
Wolfgang Barth <wob@swobspace.net>
2026-01-18 13:02:52 +0100

    model Cocard::CardCertificate

---
+++ app/models/cocard/card_certificate.rb
@@ -0,0 +1,47 @@
+module Cocard
+  class CardCertificate
+
+    ATTRIBUTES = %i( cert_ref issuer serial_number subject_name certificate
+                     expiration_date )
+
+    def initialize(card_cert_hash)
+      if card_cert_hash.has_key?(:cert_ref) and card_cert_hash.has_key?(:x509_data)
+        @hash = card_cert_hash 
+      else
+        raise "Missing card cert hash"
+      end
+    end
+
+    def cert_ref
+      hash.fetch(:cert_ref)
+    end
+
+    def issuer
+      hash.dig(:x509_data, :x509_issuer_serial, :x509_issuer_name)
+    end
+
+    def serial_number
+      hash.dig(:x509_data, :x509_issuer_serial, :x509_serial_number)
+    end
+
+    def subject_name
+      hash.dig(:x509_data, :x509_serial_number)
+    end
+
+    def x509_certificate
+      hash.dig(:x509_data, :x509_certificate)
+    end
+
+    def certificate
+      @certificate ||= Cocard::Certificate.new(x509_certificate)
+    end
+
+    def expiration_date
+      certificate.cert.not_after.to_date.to_s
+    end
+
+  private
+    attr_reader :hash
+
+  end
+end
---
+++ spec/models/cocard/card_certificate_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+module Cocard
+  RSpec.describe CardCertificate, type: :model do
+    let(:card_cert_yml) {
+      file = File.join(Rails.root, 'spec', 'fixtures', 'files', 'read_card_certificate_response.yml')
+      File.read(file)
+    }
+    let(:card_cert_hash) do 
+      YAML.unsafe_load(card_cert_yml)
+          .dig(:read_card_certificate_response, :x509_data_info_list, :x509_data_info)
+    end
+
+    subject { Cocard::CardCertificate.new(card_cert_hash) }
+
+    describe "without argument" do
+      it "::new raise an KeyError" do
+        expect { Cocard::CardCertificate.new() }.to raise_error(ArgumentError)
+      end
+    end
+
+    describe "empty certificate" do
+      it "returns nil" do
+        expect { Cocard::CardCertificate.new(nil) }.to raise_error(NoMethodError)
+      end
+    end
+   
+    describe "with real card certificate" do
+      it { expect(subject.cert_ref).to eq("C.AUT") }
+      it { expect(subject.issuer).to eq("CN=D-Trust.SMCB-CA3,OU=Institution des Gesundheitswesens-CA der Telematikinfrastruktur,O=D-TRUST GmbH,C=DE") }
+      it { expect(subject.serial_number).to eq("4683251") }
+      it { expect(subject.subject_name).to be_nil }
+      it { expect(subject.certificate).to be_kind_of Cocard::Certificate }
+      it { expect(subject.expiration_date).to eq("2026-08-15") }
+    end
+
+    describe "Cocard::CardCertificate::ATTRIBUTES" do
+      it { expect(Cocard::CardCertificate::ATTRIBUTES).to contain_exactly(:cert_ref,
+             :issuer, :serial_number, :subject_name, :certificate, :expiration_date ) }
+    end
+  end
+end
