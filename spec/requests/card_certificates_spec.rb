8a0f7e56249b70b78433ce8c3f2f3ac53d715373
Wolfgang Barth <wob@swobspace.net>
2026-01-22 16:50:01 +0100

    fix spec

--- spec/requests/card_certificates_spec.rb
+++ spec/requests/card_certificates_spec.rb
@@ -1,16 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "CardCertificates", type: :request do
+  let(:card_certificate) { FactoryBot.create(:card_certificate) }
+
+  before(:each) do
+    login_admin
+  end
+
   describe "GET /index" do
     it "returns http success" do
-      get "/card_certificates/index"
+      get card_certificates_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/card_certificates/show"
+      get card_certificate_path(card_certificate)
       expect(response).to have_http_status(:success)
     end
   end
