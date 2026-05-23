3e54fcfb8d37db998bcc941458ee804140c2a9fc
Wolfgang Barth <wob@swobspace.net>
2026-05-23 11:09:35 +0200

    documenting model oid

--- app/models/oid.rb
+++ app/models/oid.rb
@@ -1,2 +1,18 @@
 class Oid < ApplicationRecord
+  # -- associations
+  # -- configuration
+  # -- validations and callbacks
+  validates :oid, presence: true,
+                   uniqueness: { case_sensitive: false, allow_blank: false }
+  validates :name, presence: true
+
+  # -- common methods
+  def to_s
+    "#{oid} - #{name}"
+  end
+
+  def to_label
+    "#{name} (#{oid})"
+  end
+
 end
--- spec/models/oid_spec.rb
+++ spec/models/oid_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Oid, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:oid) { FactoryBot.create(:oid, oid: '1.2.3.4.5.6', name: 'Kuckuck') }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:oid) }
+
+  it 'should get plain factory working' do
+    f = FactoryBot.create(:oid)
+    g = FactoryBot.create(:oid)
+    expect(f).to be_valid
+    expect(g).to be_valid
+    expect(f).to validate_uniqueness_of(:oid).case_insensitive
+  end
+
+  describe "#to_s" do
+    it { expect(oid.to_s).to match("1.2.3.4.5.6 - Kuckuck") }
+  end
+
+  describe "#to_label" do
+    it { expect(oid.to_label).to match("Kuckuck (1.2.3.4.5.6)") }
+  end
+
 end
